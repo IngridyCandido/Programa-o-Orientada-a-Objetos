5b35f2cd11a61454b98d32a4573d74c094add8c0
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-09-12 17:52:47 -0300

    Add files via upload

---
+++ Poo/Poo/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo
+{
+    internal class Pessoa
+    {
+        public string Nome;
+        public string CPF;
+
+        public void Imprimir()
+        {
+            Console.WriteLine(Nome + " " + CPF);
+        }
+    }
+}
---
+++ Poo/Poo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Poo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pes1 = new Pessoa();
+            pes1.Nome = "Ingridy";
+            pes1.CPF = "999.999.999-99";
+            pes1.Imprimir();
+        }
+    }
+}
