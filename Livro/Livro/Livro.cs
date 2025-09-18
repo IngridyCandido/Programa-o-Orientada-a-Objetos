1a713a5cbfc5d5afb7049784b7056a02dd40bf0a
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-09-18 14:36:53 -0300

    Add files via upload

---
+++ Livro/Livro/Livro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livro
+{
+	internal class Livro
+	{
+		//atributos
+		public string titulo;
+		public string autor;
+		public int paginas;
+		public int paginaAtual;
+
+		//construtor
+		public Livro(string titulo, string autor, int paginas)
+		{
+			this.titulo = titulo;
+			this.autor = autor;
+			this.paginas = paginas;
+			this.paginaAtual = 1;
+		}
+
+		//métodos
+		public void LerPagina()
+		{
+			paginaAtual ++;
+		}
+
+		public double ObterProgresso(double porcentagemlida)
+		{
+			return (paginas / paginaAtual) * 100;
+		}
+
+		public void Reiniciar()
+		{
+			paginaAtual = 1;
+		}
+	}
+}
---
+++ Livro/Livro/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Livro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro livro1 = new Livro("Jogo dos tronos", "Joao", 345);
+            Livro livro2 = new Livro("Orgulho e Preconceito", "Jane Austin", 234);
+            Livro livro3 = new Livro("A culpa é das estralas", "John", 321);
+
+            Console.WriteLine("Os livros que temos na nossa biblioteca são: " + livro1.titulo + ", " + livro2.titulo + ", " + livro1.titulo + ".");
+        }
+    }
+}
