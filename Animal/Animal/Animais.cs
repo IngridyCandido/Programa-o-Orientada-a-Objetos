f4cc9db39d6b20b686930b45b7cf4420ba94f51d
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-09-25 14:15:52 -0300

    Add files via upload

---
+++ Animal/Animal/Animais.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Animais
+{
+	public string nome;
+	public string especie;
+	public int idade;
+	public bool estaDormindo;
+
+	public Animais (string nome, string especie, int idade)
+	{
+		this.nome = nome;
+		this.especie = especie;
+		this.idade = idade;
+		this.estaDormindo = false;
+	}
+
+	public bool Dormir (bool estaDormindo)
+	{
+		estaDormindo = true;
+		Console.WriteLine("O animal está dormindo.");
+		return estaDormindo;
+	}
+
+	public bool Acordar (bool estaDormindo)
+	{
+		estaDormindo = false;
+		Console.WriteLine("O animal acordou.");
+		return estaDormindo;
+	}
+
+	public void FazerSom()
+	{
+		Console.WriteLine("O animal fez um som.");
+    }
+
+	public void ApresentarSe(Animais Animal)
+	{
+		Console.WriteLine("Meu nome é " + Animal.nome + ". Sou da especie " + Animal.especie + " e tenho " + Animal.idade + " anos.");
+	}
+}
---
+++ Animal/Animal/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Animal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animais gato = new Animais ("Mimi", "Felino", 3);
+            Animais cachorro = new Animais ("Rex", "Canino", 5);
+            Animais passaro = new Animais ("Piu", "Ave", 1);
+
+            gato.ApresentarSe(gato);
+            gato.Dormir(false);
+            gato.Acordar(true);
+            gato.FazerSom();
+            
+
+            cachorro.ApresentarSe(cachorro);
+            cachorro.Dormir(false);
+            cachorro.Acordar(true);
+            cachorro.FazerSom();
+            
+
+            passaro.ApresentarSe(passaro);
+            passaro.Dormir(false);
+            passaro.Acordar(true);
+            passaro.FazerSom();
+            
+        }
+    }
+}
