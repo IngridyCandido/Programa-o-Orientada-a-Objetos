734fcb3f2e1ff16a7e504dacc53315e089a5a2b5
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-11-27 14:26:47 -0300

    Add files via upload

---
+++ SerializaçãoBinaria/MyObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializaçãoBinaria
+{
+    [Serializable]
+    public class MyObject
+    {
+        public int n1 {  get; set; }
+        public int n2 { get; set; }
+        public String str { get; set; }
+    }
+}
---
+++ SerializaçãoBinaria/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace SerializaçãoBinaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Serializando binariamente o objeto.");
+            //SerializarBinario();
+            //SerializarSOAP();
+            SerializarJson();
+            Console.WriteLine("Serialização concluída");
+        }
+
+        private static void SerializarBinario()
+        {
+            MyObject obj = new MyObject();
+            obj.n1 = 1;
+            obj.n2 = 24;
+            obj.str = "Aula de POO de Serialização Binária";
+            IFormatter formatter = new BinaryFormatter();
+            Stream arquivoBinario = new FileStream("MyFile.bin", FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(arquivoBinario, obj);
+            arquivoBinario.Close();
+        }
+
+        private static void SerializarSOAP()
+        {
+            MyObject obj = new MyObject();
+            obj.n1 = 1;
+            obj.n2 = 24;
+            obj.str = "Aula de POO de Serialização com Soapformatter";
+            IFormatter formatter = new SoapFormatter();
+            Stream arquivoBinario = new FileStream("MyFileSoap.xml", FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(arquivoBinario, obj);
+            arquivoBinario.Close();
+        }
+        private static void SerializarJson()
+        {
+            MyObject obj = new MyObject();
+            obj.n1 = 1;
+            obj.n2 = 24;
+            obj.str = "Aula de POO de Serialização em JsonSerializer";
+            string jsonString = JsonSerializer.Serialize(obj);
+            File.WriteAllText("MyFileJson.json", jsonString);
+        }
+    }
+}
