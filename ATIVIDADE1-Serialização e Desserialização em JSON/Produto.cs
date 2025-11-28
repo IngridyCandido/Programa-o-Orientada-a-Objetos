0f5198181a9ca1424bd444eccc0493c4f0682cc6
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-11-28 17:36:49 -0300

    Add files via upload

---
+++ ATIVIDADE1-Serialização e Desserialização em JSON/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATIVIDADE1_Serialização_e_Desserialização_em_JSON
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
---
+++ ATIVIDADE1-Serialização e Desserialização em JSON/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Text.Json;
+using System.IO;
+using System.Text.Encodings.Web;
+
+namespace ATIVIDADE1_Serialização_e_Desserialização_em_JSON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Serializando com Json...");
+            SerializarJson();
+            Console.WriteLine("Serialização concluída.");
+            Console.WriteLine("Agora ele será deserializado...");
+            DeserializarJson();
+            Console.WriteLine("Deserialização concluída.");
+        }
+        private static void SerializarJson()
+        {
+            Produto obj = new Produto();
+            obj.Id = 1;
+            obj.Nome = "Feijão";
+            obj.Preco = 3.99;
+            obj.Quantidade = 9;
+            string jsonString = JsonSerializer.Serialize(obj, new JsonSerializerOptions { WriteIndented = true, Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping });
+            File.WriteAllText("produto.json", jsonString);
+            Console.WriteLine("Objeto serializado em produto.json");
+            Console.WriteLine(jsonString);
+        }
+
+        private static void DeserializarJson()
+        {
+            string jsonLido = File.ReadAllText("produto.json");
+            Produto obj = JsonSerializer.Deserialize<Produto>(jsonLido);
+            Console.WriteLine("\nObjeto deserealizado:");
+            Console.WriteLine($"ID: {obj.Id}.");
+            Console.WriteLine($"Nome: {obj.Nome}.");
+            Console.WriteLine($"Preço: {obj.Preco}.");
+            Console.WriteLine($"Quantidade: {obj.Quantidade}.");
+        }
+    }
+}
