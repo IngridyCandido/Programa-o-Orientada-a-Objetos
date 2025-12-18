ce32a0284b5dab00bf5d1e6451c4dd6f6e7ab451
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2025-12-18 14:12:30 -0300

    Add files via upload

---
+++ AulaPOOMultiJanelas/AulaPOOMultiJanelas/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AulaPOOMultiJanelas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void novaJanelaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmNovaJanela frm = new FrmNovaJanela();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja sair?", "Fechar aplicativo", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Close();
+            }            
+        }
+    }
+}
