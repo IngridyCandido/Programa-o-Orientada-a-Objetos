1d88a4b2de83197e6485721ad97a906305b67ef8
Ingridy Candido <120406194+IngridyCandido@users.noreply.github.com>
2026-02-04 17:46:49 -0300

    Add files via upload

---
+++ Locadora/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Locadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastroDeFilmesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaDeFilmesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastroDeFilmesToolStripMenuItem,
+            this.listaDeFilmesToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "&Menu";
+            // 
+            // cadastroDeFilmesToolStripMenuItem
+            // 
+            this.cadastroDeFilmesToolStripMenuItem.Name = "cadastroDeFilmesToolStripMenuItem";
+            this.cadastroDeFilmesToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.cadastroDeFilmesToolStripMenuItem.Text = "&Cadastro de Filmes";
+            this.cadastroDeFilmesToolStripMenuItem.Click += new System.EventHandler(this.cadastroDeFilmesToolStripMenuItem_Click);
+            // 
+            // listaDeFilmesToolStripMenuItem
+            // 
+            this.listaDeFilmesToolStripMenuItem.Name = "listaDeFilmesToolStripMenuItem";
+            this.listaDeFilmesToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.listaDeFilmesToolStripMenuItem.Text = "&Lista de Filmes";
+            this.listaDeFilmesToolStripMenuItem.Click += new System.EventHandler(this.listaDeFilmesToolStripMenuItem_Click);
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.sairToolStripMenuItem.Text = "&Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cadastroDeFilmesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listaDeFilmesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+    }
+}
+
---
+++ Locadora/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Locadora
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.IsMdiContainer = true;
+        }
+
+        private void cadastroDeFilmesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CadastrodeFilmes frm1 = new CadastrodeFilmes();
+            frm1.MdiParent = this;
+            frm1.Show();
+        }
+
+        private void listaDeFilmesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ListadeFilmes frm = new ListadeFilmes();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja sair?", "Fechar aplicativo", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
